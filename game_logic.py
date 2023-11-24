b0bdaaa52f1121c0e238962698e1fb8402adf512
DominiC Mwita <dominicmwitta@gmail.com>
2023-11-24 00:23:26 +0000

    added Player class

--- game_logic.py
+++ game_logic.py
@@ -13,3 +13,59 @@ def change_turns(list_of_players, Current_Player,Next_Player):
 			new_index = 0
 		Next_Player = list_of_players[new_index]
 	return Current_Player, Next_Player
+
+
+class Player:
+    def __init__(self,name):
+        self.rack = []
+        self.name = name
+        self.turn = False # when 
+
+    
+    def initial_tiles(self,rack): 
+        # Add tiles to players rack
+        self.rack.append(rack.initial_tiles())
+    
+    # during gameplay
+    def add_tile_to_rack(self,pool):
+        # A player randomly pick two cards, select one return one
+        selected_tiles = random.sample(pool.tiles,2)
+        for tile in selected_tiles:
+            tile.show() # show tiles that a player can select, show method to be in class Tile
+            # if selected, kept in rack 
+            # otherwise return to pool
+
+        # selected_tile 
+        # for picked tile in picked tiles, the player chooses one
+        # pop the selected tile from pool
+        # not sure how to implement for human player
+
+    
+    def pick_tile_from_rack(self,tile): # does this require a parameter? unsure: idea human player clicks/or drags the tile, it captures the tile
+        index = self.rack.index(tile) if tile in self.rack else None
+        picked_tile = self.rack.pop(index)
+        return picked_tile
+    
+    # def play_tile(self):
+    #     return self.pick_tile_from_rack()
+        # board.append(tile) append tile to the board if it forms a valid set or run
+        ## add the picked tile to game board to form a set or run
+
+    # get total value of tiles in the rack: to compute total points when the game ends
+    def get_total_value(self):
+        value = 0
+        for tile in self.rack.tiles:
+            value += tile.value
+        return value
+    
+    # 
+    def pick_tiles(self, pool): 
+        selected_tiles = [pool.tiles.pop(random.randrange(len(pool.tiles))) for _ in range(2)]
+        tile1 = selected_tiles[0]
+        tile2 = selected_tiles[1]
+        return selected_tiles
+    # show selected tiles on the screen
+    # Detect mouse collision, both tiles will wait MOUSEBOTTONDOWN event, the clicked tile, added to rack
+    #   self.rack.add_tiles(selected_tiles[0 or 1])
+    # send the other tile back to the pool
+    # After this action, set players turn to False
